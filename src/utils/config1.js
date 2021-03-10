75ec44dfe7749b2599cc2c4ac494ab609b2051ce
Miroslav Lysyuk <mirolysyuk@gmail.com>
2021-03-10 16:09:46 -0600

    reorganized folder structure & css

---
+++ src/utils/config1.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  consumer_key: 'p2rnIpNPoQfqcVzJkEOT7EMbr',
+  consumer_secret: '6mkAYYPMws0V79JvieF2x1iDeau0qWlZrmnwVcspPfeNoGZM7J',
+  access_token: '30478033-4C5voiVEDJ8NQZGFRfx70Hb3oiznl708k9CDXFziJ',
+  access_token_secret: 'SLD5YxJ46daA7ARgVgCOpaMDOEX7WBctn1Uug15ZpMgpc'
+}
